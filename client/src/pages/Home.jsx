10b5a68a7054d55410b5e62778af4569c803f68e
Mohiman <61213934+mohimanb@users.noreply.github.com>
2024-09-30 17:36:21 -0400

    Text update

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -34,7 +34,7 @@ const Home = () => {
 
               <motion.div {...headContentAnimation} className="flex flex-col gap-5">
                 <p className="max-w-md font-normal text-gray-600 text-base">
-                    Elevate your fashion game with T-Designer's state-of-the-art 3D customization platform. <strong>Unleash your creative potential</strong> by crafting one-of-a-kind t-shirt designs that resonate with your unique style. From intricate logos to vibrant color palettes, our user-friendly interface empowers you to visualize and bring your sartorial visions to life. Experience the future of personalized fashion, today.
+                    Elevate your fashion game with T-Designer's state-of-the-art 3D customization platform. <strong>Unleash your maximum creative potential</strong> by crafting one-of-a-kind t-shirt designs that resonate with your unique style. From intricate logos to vibrant color palettes, our user-friendly interface empowers you to visualize and bring your sartorial visions to life. Experience the future of personalized fashion, today.
                 </p>
 
 
